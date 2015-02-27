8cd95651d8e19184cd574a05d8569837f4fd9dd5
Alexander Lysak <alexander.lysak@hp.com>
2015-02-27 10:11:24 +0200

    basic question - answer

--- src/main/java/org/xander/queryAnswer/QueryAnswer.java
+++ src/main/java/org/xander/queryAnswer/QueryAnswer.java
@@ -10,4 +10,16 @@ public class QueryAnswer {
     public String[] getData() {
         return data;
     }
+
+    public String checkValuePresent(String value) {
+        if (data.length == 0) {
+            throw new IllegalStateException("data is not filled, not able to check");
+        }
+        for (String element : data) {
+            if (value.equals(element)) {
+                return "yes";
+            }
+        }
+        return "no";
+    }
 }
--- src/test/java/org/xander/queryAnswer/QueryAnswerTest.java
+++ src/test/java/org/xander/queryAnswer/QueryAnswerTest.java
@@ -11,7 +11,6 @@ public class QueryAnswerTest {
     String[] data = {"cat", "dog", "parrot"};
     private QueryAnswer queryAnswer;
 
-
     @Before
     public void setUp(){
         queryAnswer = new QueryAnswer(data);
@@ -24,6 +23,19 @@ public class QueryAnswerTest {
 
     @Test
     public void answerIfPresent() {
+        String actualAnswer = queryAnswer.checkValuePresent("cat");
+        assertEquals("yes", actualAnswer);
+    }
 
+    @Test
+    public void answerIfNotPresent() {
+        String actualAnswer = queryAnswer.checkValuePresent("elephant");
+        assertEquals("no", actualAnswer);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void noAnswersPresent() {
+        QueryAnswer queryAnswer = new QueryAnswer(new String[]{});
+        queryAnswer.checkValuePresent("no matter what, no values anyway");
     }
 }
