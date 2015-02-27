02556fe64dc5e9fd7a5ccafd4c1be21822346e10
Alexander Lysak <alexander.lysak@hp.com>
2015-02-27 09:52:00 +0200

    initial commit

---
+++ src/main/java/org/xander/queryAnswer/QueryAnswer.java
@@ -0,0 +1,11 @@
+package org.xander.queryAnswer;
+
+/**
+ * Hello world!
+ *
+ */
+public class QueryAnswer
+{
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/org/xander/queryAnswer/QueryAnswerTest.java
@@ -0,0 +1,13 @@
+package org.xander.queryAnswer;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class QueryAnswerTest {
+    @Test
+    public void test() {
+        assertNotNull(new QueryAnswer());
+    }
+}
