29c262fd2993b154a455e45653f7cf36e12f2523
Alexander Lysak <alexander.lysak@hp.com>
2015-02-27 10:00:06 +0200

    fill in the data

--- src/main/java/org/xander/queryAnswer/QueryAnswer.java
+++ src/main/java/org/xander/queryAnswer/QueryAnswer.java
@@ -1,11 +1,13 @@
 package org.xander.queryAnswer;
 
-/**
- * Hello world!
- *
- */
-public class QueryAnswer
-{
-    public static void main(String[] args) {
+public class QueryAnswer {
+    private String[] data;
+
+    public QueryAnswer(String[] data) {
+        this.data = data;
+    }
+
+    public String[] getData() {
+        return data;
     }
 }
--- src/test/java/org/xander/queryAnswer/QueryAnswerTest.java
+++ src/test/java/org/xander/queryAnswer/QueryAnswerTest.java
@@ -1,13 +1,29 @@
 package org.xander.queryAnswer;
 
-
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class QueryAnswerTest {
+    String[] data = {"cat", "dog", "parrot"};
+    private QueryAnswer queryAnswer;
+
+
+    @Before
+    public void setUp(){
+        queryAnswer = new QueryAnswer(data);
+    }
+
     @Test
-    public void test() {
-        assertNotNull(new QueryAnswer());
+    public void fillTheData() {
+        assertArrayEquals(data, queryAnswer.getData());
+    }
+
+    @Test
+    public void answerIfPresent() {
+
     }
 }
